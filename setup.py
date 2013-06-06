f847d8a6a506af941bdcb0592ff41230640ef8ed
ant30 <ant30tx@gmail.com>
2013-06-06 11:49:08 +0200

    Fix setup.py styles, pyramid versions

--- setup.py
+++ setup.py
@@ -6,19 +6,22 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-requires=['pyramid>=1.0.2', 'pyramid_jinja2']
+requires = [
+    'pyramid==1.4',
+    'pyramid_jinja2==1.6',
+]
 
 setup(name='eduid-dashboard',
       version='0.0',
       description='eduid-dashboard',
-      long_description=README + '\n\n' +  CHANGES,
+      long_description=README + '\n\n' + CHANGES,
       classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pylons",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='',
+          "Programming Language :: Python",
+          "Framework :: Pylons",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='NORDUnet A/S',
       author_email='',
       url='',
       keywords='web pyramid pylons',
@@ -28,7 +31,7 @@ setup(name='eduid-dashboard',
       install_requires=requires,
       tests_require=requires,
       test_suite="eduiddashboard",
-      entry_points = """\
+      entry_points="""\
       [paste.app_factory]
       main = eduiddashboard:main
       """,
