312600dd4b2fa86543085861c04923b67ae45bba
ant30 <ant30tx@gmail.com>
2013-06-12 10:51:29 +0200

    Remove from form hidden fields

--- eduiddashboard/models.py
+++ eduiddashboard/models.py
@@ -1,20 +1,9 @@
 import colander
 
 
-class Password(colander.MappingSchema):
-    id = colander.SchemaNode(colander.String())
-    salt = colander.SchemaNode(colander.String())
-
-
-class Passwords(colander.SequenceSchema):
-    passwords = Password()
-
-
 class Person(colander.MappingSchema):
-    _id = colander.SchemaNode(colander.String())
     first_name = colander.SchemaNode(colander.String())
     last_name = colander.SchemaNode(colander.String())
     screen_name = colander.SchemaNode(colander.String())
     email = colander.SchemaNode(colander.String(),
                                 validator=colander.Email())
-    passwords = Passwords()
