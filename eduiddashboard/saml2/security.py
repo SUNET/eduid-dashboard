bd040da71d639345f12e25b277b2ba6252d1e920
ant30 <ant30tx@gmail.com>
2013-06-14 14:51:22 +0200

    Remove an unused file

--- eduiddashboard/saml2/security.py
+++
@@ -1,9 +0,0 @@
-from pyramid.security import authenticated_userid
-
-
-# Only authentication is required for edit the user profile
-def groupfinder(userid, request):
-    if authenticated_userid(request):
-        return ['group:owner']
-    else:
-        return []
