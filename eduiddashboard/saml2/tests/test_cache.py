56ac573337feb0806e86762bb846f6bda8d34824
ant30 <ant30tx@gmail.com>
2013-06-19 09:59:40 +0200

    Implemented tests for saml pyramidSessionCacheAdapter

--- eduiddashboard/saml2/tests/test_cache.py
+++ eduiddashboard/saml2/tests/test_cache.py
@@ -17,10 +17,35 @@ class pyramidSessionCacheAdapterTests(unittest.TestCase):
         self.assertEqual(psca.key, psca.key_prefix + 'saml2')
 
     def test_get_objects(self):
-        pass
+        fake_session_dict = {
+            'user': 'someone@example.com',
+        }
+        psca = pyramidSessionCacheAdapter(fake_session_dict, 'saml2')
+
+        self.assertEqual(psca._get_objects(), {})
 
     def test_set_objects(self):
-        pass
+        fake_session_dict = {
+            'user': 'someone@example.com',
+        }
+        psca = pyramidSessionCacheAdapter(fake_session_dict, 'saml2')
+
+        psca._set_objects({
+            'onekey': 'onevalue',
+        })
+
+        self.assertEqual(psca._get_objects(), {'onekey': 'onevalue'})
 
     def test_sync(self):
-        pass
+        fake_session_dict = {
+            'user': 'someone@example.com',
+        }
+        psca = pyramidSessionCacheAdapter(fake_session_dict, 'saml2')
+
+        psca.sync()
+        self.assertEqual(psca._get_objects(), {})
+
+        psca['onekey'] = 'onevalue'
+
+        psca.sync()
+        self.assertEqual(psca._get_objects(), {'onekey': 'onevalue'})
