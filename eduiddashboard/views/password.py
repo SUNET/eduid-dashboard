4459ec1265a0fab04b191ad0dd9ba496424540cf
ant30 <ant30tx@gmail.com>
2013-08-20 12:48:01 +0200

    Refacts to use pyramid_deform

---
+++ eduiddashboard/views/password.py
@@ -0,0 +1,57 @@
+## Passwords form
+
+from pyramid.view import view_config
+from bson import ObjectId
+
+import vccs_client
+
+from eduid_am.tasks import update_attributes
+
+from eduiddashboard.i18n import TranslationString as _
+from eduiddashboard.models import Passwords
+
+from eduiddashboard.views import BaseFormView
+
+
+@view_config(route_name='passwords', permission='edit',
+             renderer='templates/passwords-form.jinja2')
+class PasswordsView(BaseFormView):
+    """
+    Change user passwords
+        * GET = Rendering template
+        * POST = Creating or modifing personal data,
+                    return status and flash message
+    """
+
+    schema = Passwords()
+    route = 'passwords'
+
+    def submit_success(self, user_modified):
+
+        passwords = self.schema.serialize(user_modified)
+        new_password = passwords['new_password']
+        old_password = passwords['old_password']
+        email = self.user['email']
+
+        password_id = ObjectId()
+
+        vccs = vccs_client.VCCSClient(
+            base_url=self.request.registry.settings.get('vccs_url'),
+        )
+        old_factor = vccs_client.VCCSPasswordFactor(old_password,
+                                                    credential_id=str(password_id))
+        if not vccs.authenticate(email, [old_factor]):
+            # TODO: include validation errors into form
+            self.request.session.flash(_('ERROR: Your old password do not match'),
+                                       queue='forms')
+            return self.context
+
+        new_factor = vccs_client.VCCSPasswordFactor(new_password,
+                                                    credential_id=str(password_id))
+        vccs.add_credentials(email, [new_factor])
+
+        self.request.session.flash(_('Your changes was saved, please, wait '
+                                     'before your changes are distributed '
+                                     'through all applications'),
+                                   queue='forms')
+        return self.context
