60aa38bd787d9611f0fbd145afea45191a6328cf
ant30 <ant30tx@gmail.com>
2013-08-29 08:53:36 +0200

    Validate that email doesn't exist only in adding operations

--- eduiddashboard/validators.py
+++ eduiddashboard/validators.py
@@ -8,9 +8,12 @@ class EmailUniqueValidator(object):
     def __call__(self, node, value):
 
         request = node.bindings.get('request')
-        emails = request.session.get('user', {}).get('emails')
 
-        for email in emails:
-            if value == email['email']:
-                raise colander.Invalid(node, _("This email is already "
-                                       "registered in your profile"))
+        if 'add' in request.POST:
+
+            emails = request.session.get('user', {}).get('emails')
+
+            for email in emails:
+                if value == email['email']:
+                    raise colander.Invalid(node, _("This email is already "
+                                           "registered in your profile"))
