4351c60168169518483523136d2618cd6a296ab6
Manuel Saelices <msaelices@yaco.es>
2013-10-09 11:52:57 +0200

    Default expiration timeout setting: 30 minutes.

--- eduiddashboard/verifications.py
+++ eduiddashboard/verifications.py
@@ -14,7 +14,7 @@ def dummy_message(request, message):
 
 
 def get_verification_code(request, model_name, obj_id):
-    expiration_timeout = request.registry.settings.get('verification_code_timeout')
+    expiration_timeout = request.registry.settings.get('verification_code_timeout', 30)
     expire_limit = datetime.now(utc) - timedelta(minutes=int(expiration_timeout))
     result = request.db.verifications.find_one({
         'obj_id': obj_id,
