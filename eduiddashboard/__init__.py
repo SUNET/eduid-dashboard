9ebcde403c401ff738e834609543531ed2fbf200
ant30 <ant30tx@gmail.com>
2013-10-09 12:48:09 +0200

    Fix the merge result

--- eduiddashboard/__init__.py
+++ eduiddashboard/__init__.py
@@ -130,51 +130,6 @@ def jinja2_settings(settings):
     """)
 
 
-def read_permissions(raw):
-    if raw.strip() == '':
-        return REQUIRED_GROUP_PER_WORKMODE
-
-    rows = raw.split('\n')
-
-    permissions = {}
-
-    for row in rows:
-        splitted_row = row.split('=')
-        workmode = row.split('=')[0].strip()
-        if len(splitted_row) > 1:
-            urn = row.split('=')[1].strip()
-        else:
-            urn = ''
-        if workmode in AVAILABLE_WORK_MODES:
-            permissions[workmode] = urn
-
-    return permissions
-
-
-def read_proofing_links(settings):
-
-    raw_proofing_links = read_setting_from_env(settings, 'proofing_links', '')
-
-    proofing_links = {}
-    added_links = []
-    for line in raw_proofing_links.split('\n'):
-        if not line.strip():
-            continue
-        (proofing_component, url) = line.split('=')
-        proofing_component = proofing_component.strip()
-
-        url = url.strip()
-        if (proofing_component in REQUIRED_PROOFING_LINKS and
-                proofing_component not in added_links):
-            proofing_links[proofing_component] = url
-            added_links.append(proofing_component)
-
-    if len(added_links) != len(REQUIRED_PROOFING_LINKS) and not 'testing' in settings:
-        raise ConfigurationError('The proofing_links configuration is not OK')
-
-    return proofing_links
-
-
 def add_custom_deform_templates_path():
     templates_path = 'templates/form-widgets'
     try:
@@ -362,8 +317,6 @@ def main(global_config, **settings):
 
     if settings['proofing_links'] is None:
         raise ConfigurationError('The proofing_links configuration is not OK')
-
-    settings['proofing_links'] = read_proofing_links(settings)
 
     settings['groups_callback'] = read_setting_from_env(settings,
                                                         'groups_callback',
