9b267f5025b354b214bfe0f96f14fe06cbaf17f5
Fredrik Thulin <fredrik@thulin.net>
2013-10-17 10:59:02 +0200

    Not sure if this is the reason we now get lots of Forbidden...
    Revert "check that user was found in more cases"
    
    This reverts commit 48f3cfaab31c8d12f1d9cd3b5c213696f6bdcf91.

--- eduiddashboard/permissions.py
+++ eduiddashboard/permissions.py
@@ -105,11 +105,8 @@ class BaseFactory(object):
                 user = self.request.userdb.get_user(userid)
             elif OID_RE.match(userid):
                 user = self.request.userdb.get_user_by_oid(userid)
-        # this 'if not user' used to have one more level of indentation -
-        # don't know for sure if it is the right thing to move it, but I
-        # have reason to suspect it is... - ft 2013-10-16
-        if not user:
-            raise HTTPForbidden(_('User unknown'))
+            if not user:
+                raise HTTPNotFound()
         self._user = user
         return user
 
