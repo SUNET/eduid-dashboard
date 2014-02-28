781f6b06244b676515c5df6caada408f900a74e0
Johan Lundberg <lundberg@nordu.net>
2014-02-28 13:13:08 +0100

    Pre merge with master

--- eduiddashboard/views/admin.py
+++ eduiddashboard/views/admin.py
@@ -12,12 +12,15 @@ logger = logging.getLogger(__name__)
 # Uses the userid (user_main_attribute) for all fields except _id where
 # the user objects _id in the same database will be used.
 DATABASES = {
-    'eduid_dashboard': [
-        {'name': 'profiles', 'field': 'mail'},
-        {'name': 'verifications', 'field': '_id'}
-    ],
     'eduid_am': [
         {'name': 'attributes', 'field': 'mail'}
+    ],
+    'eduid_dashboard': [
+        {'name': 'profiles', 'field': 'mail'},
+        {'name': 'verifications', 'field': 'user_oid'}
+    ],
+    'eduid_signup': [
+        {'name': 'registered', 'field': 'email'}
     ]
 }
 
@@ -32,6 +35,7 @@ def get_databases():
             'db': Database(MongoClient('localhost', 27017), u'eduid_am')},
         }, ...
     }
+    :return dict with dbs and collection info
     """
     dbs = {}
     for item in DATABASES:
@@ -53,20 +57,22 @@ def admin_status(context, request):
     if userid:
         for dbname in dbs:
             userdata[dbname] = {}
+            oid = None
             for collection in dbs[dbname]['collections']:
-                if collection['field'] == '_id':
-                    pass
-                else:
-                    try:
+                try:
+                    if collection['field'] == 'user_oid':
+                        if oid:
+                            hit = dbs[dbname]['db'][collection['name']].find({collection['field']: oid})[0]
+                        else:
+                            hit = {'error': 'No OID to look up.'}
+                    else:
                         hit = dbs[dbname]['db'][collection['name']].find({collection['field']: userid})[0]
-                    except IndexError:
-                        hit = {}
-                    userdata[dbname][collection['name']] = hit
+                        oid = hit['_id']
+                except IndexError:
+                    hit = {'error': 'No user found.'}
+                userdata[dbname][collection['name']] = hit
 
 
     return {
         'userdata': userdata,
-        # DEBUG
-        'dir_context': dir(context),
-        'dir_request': dir(request)
     }
