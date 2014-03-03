ad3b5c282ab8af1a2b66874867eeb221297b53c9
Fredrik Thulin <fredrik@thulin.net>
2014-03-03 20:04:40 +0100

    eduiddashboard.db is no more

--- eduiddashboard/views/admin.py
+++ eduiddashboard/views/admin.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'lundberg'
 
-from eduiddashboard.db import MongoDB
 from pyramid.view import view_config
 
 from eduiddashboard.i18n import TranslationString as _
