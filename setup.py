76ffa21110e41eb9ba812ecd621839d1774a8791
Stefan Wold <swold@sunet.se>
2014-03-28 08:31:09 +0100

    Version 0.1.6

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ waitress_extras = requires + [
 ]
 
 setup(name='eduid-dashboard',
-      version='0.1.6-dev',
+      version='0.1.6',
       description='eduid-dashboard',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
