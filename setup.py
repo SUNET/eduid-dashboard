d49bc3c1b89294497fc92812ba376261d8096d66
Stefan Wold <swold@sunet.se>
2014-03-28 08:32:07 +0100

    Next dev iteration (0.1.7-dev)

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ waitress_extras = requires + [
 ]
 
 setup(name='eduid-dashboard',
-      version='0.1.6',
+      version='0.1.7-dev',
       description='eduid-dashboard',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
