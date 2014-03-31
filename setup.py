936ef7599c3ff92d673b430a7e6271e5c1322975
Fredrik Thulin <fredrik@thulin.net>
2014-03-31 15:40:06 +0200

    version 0.1.9

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ waitress_extras = requires + [
 ]
 
 setup(name='eduid-dashboard',
-      version='0.1.9-dev',
+      version='0.1.9',
       description='eduid-dashboard',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
