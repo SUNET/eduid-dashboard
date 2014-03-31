28b1195f64192af5f6a9e0f55e13da59f22b8e6d
Fredrik Thulin <fredrik@thulin.net>
2014-03-31 16:15:51 +0200

    change version to 0.1.11-dev

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-version = '0.1.10'
+version = '0.1.11-dev'
 
 requires = [
     'eduid_am==0.4.3',
