9d646329cf227b249b9b2bd2cd8018640b8739a2
Fredrik Thulin <fredrik@thulin.net>
2014-03-31 16:08:12 +0200

    version 0.1.10

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
+version = '0.1.10'
+
 requires = [
     'eduid_am==0.4.3',
     'pymongo==2.6.3',
@@ -73,7 +75,7 @@ waitress_extras = requires + [
 ]
 
 setup(name='eduid-dashboard',
-      version='0.1.10-dev',
+      version=version,
       description='eduid-dashboard',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
