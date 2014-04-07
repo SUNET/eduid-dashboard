8af1fc01a4972ae919f5a8335eb15503b4bb3cf4
Stefan Wold <swold@sunet.se>
2014-04-07 13:49:55 +0200

    Next dev iteration (0.1.14-dev)

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-version = '0.1.13'
+version = '0.1.14-dev'
 
 requires = [
     'eduid_am==0.4.5',
