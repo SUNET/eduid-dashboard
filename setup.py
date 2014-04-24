dab9648c5ec28edb4cd724c54f037542091d35a6
Stefan Wold <swold@sunet.se>
2014-04-24 14:56:01 +0200

    Bump eduid_msg and tag new release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-version = '0.1.16'
+version = '0.1.17'
 
 requires = [
     'eduid_am==0.4.6',
@@ -43,7 +43,7 @@ requires = [
     'vccs_client==0.4.1',
     'pwgen==0.4',
     'zxcvbn==1.0',
-    'eduid_msg==0.7.9',
+    'eduid_msg==0.7.10',
     'pytz'
 ]
 
