0da448dc6192ef82c099a2ed0850a978cc4ad813
Fredrik Thulin <fredrik@thulin.net>
2014-11-20 12:09:42 +0100

    new version 0.3.0

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-version = '0.3.0-dev'
+version = '0.3.0'
 
 requires = [
-    'eduid_am==0.5.0-dev',
+    'eduid_am==0.5.0',
     'pymongo==2.6.3',
     'pyramid==1.4.1',
     'pyramid_jinja2==1.6',
