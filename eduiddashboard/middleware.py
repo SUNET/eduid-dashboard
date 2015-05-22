761a2cbbe8ffc86a9cacef484049369991a28af4
Enrique Pérez Arnaud <enrique@cazalla.net>
2015-05-22 10:45:01 +0200

    remove user synchronization middleware

--- eduiddashboard/middleware.py
+++ eduiddashboard/middleware.py
@@ -1,10 +1,4 @@
 
-from pyramid.response import Response
-from pyramid.i18n import get_localizer
-from pyramid.httpexceptions import HTTPFound
-
-from eduiddashboard.i18n import TranslationString as _
-
 import logging
 log = logging.getLogger(__name__)
 
@@ -34,36 +28,3 @@ def reauthn_ts_tween_factory(handler, registry):
         return response
 
     return clean_reauthn_ts_from_session
-
-
-def sync_user_tween_factory(handler, registry):
-
-    def sync_user(request):
-        '''
-        '''
-        if request.method == 'POST':
-            user = request.session.get('edit-user', None)
-            if user is None:
-                user = request.session.get('user', None)
-            if user is not None:
-                in_sync = request.userdb.in_sync(user)
-                if not in_sync:
-                    msg = _('The user was out of sync. Please try again.')
-                    tr_msg = get_localizer(request).translate(msg)
-                    if request.get('HTTP_X_REQUESTED_WITH', False):
-                        msg = json.dumps({
-                                'result': 'out_of_sync',
-                                'message': tr_msg,
-                            })
-                        return Response(
-                            body=json.dumps(msg),
-                            status='200 Ok',
-                            content_type='application/json; charset=UTF-8')
-                    else:
-                        request.session.flash(tr_msg)
-                        return HTTPFound(location=request.route_url('profile-editor'))
-
-        return handler(request)
-
-    return sync_user
-
