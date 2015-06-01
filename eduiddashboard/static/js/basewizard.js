ea9e2d7112a7715114f566722ba1549fc5f37125
Enrique Pérez Arnaud <enrique@cazalla.net>
2015-06-01 13:18:53 +0200

    fix responsive wizard for the new design

--- eduiddashboard/static/js/basewizard.js
+++ eduiddashboard/static/js/basewizard.js
@@ -17,6 +17,7 @@
             showCancel: true,
             isModal: true,
             submitUrl: path,
+            contentHeight: 400,
             buttons: {
               submitText: msg_done,
               submittingText: msg_sending,
