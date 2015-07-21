6db7fbff87310726bb87732804dd51c0835dc949
John Van de Meulebrouck Brendgard <john@nordu.net>
2015-07-21 16:03:06 +0200

    Added checks of cookies and fixed syntax

--- eduiddashboard/tests/test_locales.py
+++ eduiddashboard/tests/test_locales.py
@@ -11,10 +11,9 @@ class LocaleChangeTests(LoggedInReguestTests):
 
     def test_get_default_lang(self):
         self.set_logged(user='johnsmith@example.com')
-        response = self.testapp.get('/profile/')
+        response = self.testapp.get('/profile/', status=200)
         lang_name = read_mapping(self.settings, 'available_languages')[self.default_language]
-        self.assertIn('<span>{0}</span>'.format(lang_name),
-                      response.body)
+        self.assertIn('<span>{0}</span>'.format(lang_name), response.body)
 
     def test_change_language_with_referer(self):
         self.set_logged(user='johnsmith@example.com')
@@ -26,11 +25,10 @@ class LocaleChangeTests(LoggedInReguestTests):
                                         'HTTP_HOST': host
                                     },
                                     status=302)
-        self.assertIsNotNone(response.cookies_set.get('lang', 'sv'))
+        cookies = self.testapp.cookies
+        self.assertIsNotNone(cookies.get('lang', None))
+        self.assertEqual('sv', cookies.get('lang', None))
         self.assertEqual(referer, response.location)
-        response = self.testapp.get(referer, status=200)
-        lang_name = read_mapping(self.settings, 'available_languages')['sv']
-        self.assertIn('<span>{0}</span>'.format(lang_name), response.body)
 
     def test_change_language_with_invalid_referer(self):
         self.set_logged(user='johnsmith@example.com')
@@ -43,7 +41,9 @@ class LocaleChangeTests(LoggedInReguestTests):
                                         'HTTP_HOST': host
                                     },
                                     status=302)
-        self.assertIsNotNone(response.cookies_set.get('lang', 'sv'))
+        cookies = self.testapp.cookies
+        self.assertIsNotNone(cookies.get('lang', None))
+        self.assertEqual('sv', cookies.get('lang', None))
         self.assertEqual(dashboard_baseurl, response.location)
 
     def test_change_language_with_invalid_host(self):
@@ -58,20 +58,32 @@ class LocaleChangeTests(LoggedInReguestTests):
                                         'HTTP_HOST': invalid_host
                                     },
                                     status=302)
-        self.assertIsNotNone(response.cookies_set.get('lang', 'sv'))
+        cookies = self.testapp.cookies
+        self.assertIsNotNone(cookies.get('lang', None))
+        self.assertEqual('sv', cookies.get('lang', None))
         self.assertEqual(dashboard_baseurl, response.location)
 
     def test_change_language_without_referer(self):
         self.set_logged(user='johnsmith@example.com')
         response = self.testapp.get('/set_language/?lang=sv', status=302)
         response = self.testapp.get('/profile/', status=200)
+        cookies = self.testapp.cookies
+        self.assertIsNotNone(cookies.get('lang', None))
+        self.assertEqual('sv', cookies.get('lang', None))
         self.assertIn('<span>Svenska</span>', response.body)
 
     def test_language_cookie(self):
         self.set_logged(user='johnsmith@example.com')
         self.testapp.set_cookie('lang', 'sv')
-        response = self.testapp.get('/profile/',
-                                    status=200)
+        response = self.testapp.get('/profile/', status=200)
+        cookies = self.testapp.cookies
+        self.assertIsNotNone(cookies.get('lang', None))
+
+        # The cookie is escaped when setting the value manually
+        # and therefore our assertion have to use the same
+        # escaping around the value when testing it.
+        self.assertEqual('"sv"', cookies.get('lang', None))
+
         self.assertIn('<span>Svenska</span>', response.body)
 
     def test_change_language_not_available(self):
