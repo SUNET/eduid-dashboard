fbd223718d512ae376173cf8a3d5f73c9e858a41
Johan Lundberg <lundberg@nordu.net>
2015-09-25 15:59:22 +0200

    Added passwords to test user

--- eduiddashboard/tests/test_views_password.py
+++ eduiddashboard/tests/test_views_password.py
@@ -309,7 +309,11 @@ TEST_USER = {
         }, {
             'email': 'johnnysmith3@example.com',
             'verified': False,
-        }]
+        }],
+        'passwords': [{
+            'id': ObjectId('112345678901234567890123'),
+            'salt': '$NDNv1H1$9c810d852430b62a9a7c6159d5d64c41c3831846f81b6799b54e1e8922f11545$32$32$',
+        }],
     }
 
 TEST_VERIFICATIONS = [{
