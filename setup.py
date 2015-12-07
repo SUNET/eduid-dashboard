bce56256aa6cf8117fd7bf5249eff71ddd031883
Johan Lundberg <lundberg@nordu.net>
2015-12-07 14:15:37 +0100

    Bumped version 0.4.0 -> 0.4.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-version = '0.4.0'
+version = '0.4.1'
 
 requires = [
     'eduid_am>=0.6.0',
