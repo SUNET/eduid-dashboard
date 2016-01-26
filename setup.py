dc72bedfc7473cafb44c17c889604b67df76da44
Fredrik Thulin <fredrik@thulin.net>
2016-01-26 09:19:38 +0100

    try using the webapp flavor of eduid-common

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ requires = [
     'vccs_client >= 0.4.1',
     'eduid_lookup_mobile >= 0.0.4',
     'eduid_userdb >= 0.0.2',
-    'eduid-common > 0.1.2',
+    'eduid-common[webapp] > 0.1.2',
     'pysaml2 == 1.2.0beta2',
 
     'pymongo>=2.8,<3',
