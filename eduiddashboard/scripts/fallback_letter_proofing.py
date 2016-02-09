a80965e3dcef00b9f895a2302302480fb25db230
Johan Lundberg <lundberg@nordu.net>
2016-02-09 14:25:02 +0100

    Do not try to use sessions.

--- eduiddashboard/scripts/fallback_letter_proofing.py
+++ eduiddashboard/scripts/fallback_letter_proofing.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import json
 
 from eduid_userdb.exceptions import UserOutOfSync
-from eduiddashboard.verifications import get_verification_code, verify_code
+from eduiddashboard.verifications import get_verification_code, verify_nin
 from eduiddashboard.idproofinglog import LetterProofing
 
 import logging
@@ -16,7 +16,46 @@ logger = logging.getLogger(__name__)
 
 __author__ = 'lundberg'
 
+"""
+Verify a NIN for a letter verification that has failed due to a worker timeout.
+"""
+
 default_config_file = '/opt/eduid/eduid-dashboard/etc/eduid-dashboard.ini'
+env = bootstrap(default_config_file)
+
+
+def verify_code(user, verification_doc):
+    """
+    :param user: Dashboard user
+    :type user:
+    :param verification_doc: Mongo doc
+    :type verification_doc: dict
+    """
+    reference = unicode(verification_doc['_id'])
+    obj_id = verification_doc['obj_id']
+
+    if not obj_id:
+        print "Could not find nin in verification object {!r}".format(verification_doc)
+        sys.exit(1)
+
+    assert_error_msg = 'Requesting users ID does not match verifications user ID'
+    assert user.get_id() == verification_doc['user_oid'], assert_error_msg
+
+    user, msg = verify_nin(env['request'], user, obj_id, reference)
+
+    try:
+        user.save(env['request'])
+        print "Verified NIN saved for user {!r}.".format(user)
+        verified = {
+            'verified': True,
+            'verified_timestamp': datetime.utcnow()
+        }
+        verification_doc.update(verified)
+        env['request'].db.verifications.update({'_id': verification_doc['_id']}, verification_doc)
+        print "Verifications doc ({!s}) marked as verified".format(reference)
+    except UserOutOfSync:
+        print "Verified NIN NOT saved for user {!r}. User out of sync.".format(user)
+        sys.exit(1)
 
 
 def letter_proof_user():
@@ -35,7 +74,7 @@ def letter_proof_user():
         print('Two arguments required')
         print(usage)
         return 2
-    env = bootstrap(default_config_file)
+
     eppn = args[0]
     data = args[1]
     rdata = json.loads(data)
@@ -58,11 +97,12 @@ def letter_proof_user():
         print "Logging proofing data for user {!r}.".format(user)
         if env['request'].idproofinglog.log_verification(proofing_data):
             print "Finished logging proofing data for user {!r}.".format(user)
-            code_data = get_verification_code(env['request'], 'norEduPersonNIN', obj_id=rdata['number'], user=user)
+            verification_doc = get_verification_code(env['request'], 'norEduPersonNIN', obj_id=rdata['number'],
+                                                     user=user)
             try:
                 # This is a hack to reuse the existing proofing functionality, the users code is
                 # verified by the micro service
-                verify_code(env['request'], 'norEduPersonNIN', code_data['code'])
+                verify_code(user, verification_doc)
                 print "Verified NIN by physical letter saved for user {!r}.".format(user)
             except UserOutOfSync:
                 print "Verified NIN by physical letter NOT saved for user {!r}. User out of sync.".format(user)
