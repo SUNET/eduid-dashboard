f209c000ff3e68b7dd8f54f72d2586b74b0c9423
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-04-05 11:46:09 +0200

    fix mobile test

--- eduiddashboard/tests/test_views_mobiles.py
+++ eduiddashboard/tests/test_views_mobiles.py
@@ -174,7 +174,8 @@ class MobilesFormTests(LoggedInRequestTests):
             {'identifier': 1, 'code': '9d392c', 'action': 'verify'}
         )
         userdb_after = self.db.profiles.find({'_id': self.user['_id']})[0]
-        verified_mobile = userdb_after['mobile'][1]
+        from nose.tools import set_trace;set_trace()
+        verified_mobile = userdb_after['phone'][1]
         self.assertEqual(verified_mobile['verified'], True)
 
     def test_setprimary_nonexistent_mobile(self):
